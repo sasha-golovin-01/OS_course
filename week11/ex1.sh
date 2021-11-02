d00bfbb6c8ecd03bf4efacd095a12587583408f1
sasha-golovin-01 <55413919+sasha-golovin-01@users.noreply.github.com>
2021-11-02 22:59:40 +0300

    Add files via upload

--- week11/ex1.sh
+++ week11/ex1.sh
@@ -1,1 +1,7 @@
-!#/bin/bash
+dd if=/dev/zero of=lofs.img bs=1M count=50
+sudo losetup -fP lofs.iiimg
+sudo losetup -a
+sudo mkfs.ext4 ./lofsi.img
+mkdir lofsdisk
+sudo mount -o loop /dev/loop5 ./lofsdisk/
+df -hP lofsdisk
